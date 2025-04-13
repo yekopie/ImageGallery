4befc7684692f995e62677d2d3451b77862fea68
yekopie <yekuniverse@gmail.com>
2025-04-13 20:19:19 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# 🖼️ Basit Resim Galerisi
+Sayfamızda toplamda 33 adet rastgele olarak getirilmişresim bulunmaktadır
+Herhangi bir görsele tıkladığında, o görsel büyük haliyle modal içinde açılıyor.
+
+Basit, kullanışlı ve öğretici 👌
+
+---
+
+## 📸 Ekran Görüntüleri
+
+<div align="center">
+  <img src="screenshots/FullScreen.png" alt="Ana Grid Yapısı" width="600"/>
+  <br><br>
+  <img src="screenshots/OpenedModal.png" alt="Açılan Modal Görünümü" width="600"/>
+</div>
+
+---
+
+## 🛠️ Kullanılan Teknolojiler
+
+- HTML & CSS (Grid yapısı)
+- JavaScript (dinamik kutular ve görseller)
+- Bootstrap 5 (modal yapısı için)
+- [Picsum.photos](https://picsum.photos) (görselleri rastgele çekmek için)
+
+---
+## Kurulum
+Bu projeyi yerel ortamınızda çalıştırmak için:
+
+   git clone https://https://github.com/yekopie/Cv-creator
+
+projeyi klonlayıp Html'i açmanız yeterlidir.
+
+## 👨‍💻 Geliştirici
+---
+Bu proje [**Yunus Emre Kalaycı**](https://github.com/yekopie) tarafından geliştirilmiştir.  
+Fikir, sade ama şık bir görsel galeri oluşturmak 💡
+
+---
+
+Ziyaretiniz için teşekkür ederim ☺️
---
+++ index.js
@@ -0,0 +1,46 @@
+const grid = document.getElementById("grid");
+let num = 1;
+
+for (let row = 1; row <= 6; row++) {
+    for (let col = 1; col <= 6; col++) {
+        // Ortadaki 2x2 alanı geçiyoruz
+        if ((row === 3 || row === 4) && (col === 3 || col === 4)) {
+            continue;
+        }
+
+        const box = document.createElement("div");
+        box.classList.add("box");
+
+        const img = document.createElement("img");
+        img.src = `https://picsum.photos/900/900?random=${num++}`;
+        
+        // 👇 Her resme tıklanınca modal açılır ve resim gösterilir
+        img.addEventListener("click", () => {
+            const modalImg = document.getElementById("modalImage");
+            modalImg.src = img.src;
+            const modal = new bootstrap.Modal(document.getElementById("imageModal"));
+            modal.show();
+        });
+
+        box.appendChild(img);
+        grid.appendChild(box);
+    }
+}
+
+// Ortadaki büyük kutu
+const bigBox = document.createElement("div");
+bigBox.classList.add("box", "big");
+
+const bigImg = document.createElement("img");
+bigImg.src = `https://picsum.photos/900/900?random=${num}`;
+
+// Ortadaki büyük kutuya event ekliyoruz
+bigImg.addEventListener("click", () => {
+    const modalImg = document.getElementById("modalImage");
+    modalImg.src = bigImg.src;
+    const modal = new bootstrap.Modal(document.getElementById("imageModal"));
+    modal.show();
+});
+
+bigBox.appendChild(bigImg);
+grid.appendChild(bigBox);
